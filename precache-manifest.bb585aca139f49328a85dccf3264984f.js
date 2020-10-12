d3338583f3df47b1ca8504b5cecbc41641fc9b72
Shivam Ojha <shiv2000dmb@gmail.com>
2020-10-12 17:32:47 +0530

    Updates

---
+++ precache-manifest.bb585aca139f49328a85dccf3264984f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "034ed445896e3bec2fc8ecb18825213c",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "0cb655d61ebcf340688c",
+    "url": "/Todo-App/static/css/main.127e086c.chunk.css"
+  },
+  {
+    "revision": "62cab7ba6667d58db9a9",
+    "url": "/Todo-App/static/js/2.fb5f6982.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/Todo-App/static/js/2.fb5f6982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb655d61ebcf340688c",
+    "url": "/Todo-App/static/js/main.27d0431e.chunk.js"
+  },
+  {
+    "revision": "3f729d55153d06827478",
+    "url": "/Todo-App/static/js/runtime-main.7e7dbe5e.js"
+  }
+]);
